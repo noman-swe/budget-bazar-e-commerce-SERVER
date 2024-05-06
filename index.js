a138cea5ebb3d390096b473b5c775f58abb9e123
noman-swe <noman.swe130@gmail.com>
2024-05-06 12:32:03 +0600

    Server CRUD done

---
+++ index.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// database set-up starts:
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.xmq0nwv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        const productsCollection = client.db('emazone-ecommerce').collection('products');
+        console.log("Connected to MongoDB!");
+
+        // CRUD 
+        // READ :
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = productsCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+        // READ EACH :
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const product = await productsCollection.findOne(query);
+            res.send(product);
+        });
+        // CREATE :
+        app.post('/products', async (req, res) => {
+            const newProduct = req.body;
+            const result = await productsCollection.insertOne(newProduct);
+            res.send(result);
+        });
+        // DELETE :
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await productsCollection.deleteOne(query);
+            res.send(result);
+        });
+        // UPDATE :
+        app.put('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedProduct = req.body;
+            const query = { _id: new ObjectId(id) };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    name: updatedProduct.name,
+                    category: updatedProduct.category,
+                    price: updatedProduct.price,
+                    stock: updatedProduct.stock,
+                    ratings: updatedProduct.ratings,
+                    ratingsCont: updatedProduct.ratingsCount,
+                    shipment: updatedProduct.shipment,
+                    img: updatedProduct.img,
+                    seller: updatedProduct.seller,
+                    quantity: updatedProduct.quantity,
+
+                }
+            }
+            const result = await productsCollection.updateOne(query, updatedDoc, options);
+            res.send(result);
+        });
+
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+// database set-up ends.
+
+
+app.get('/', (req, res) => { res.send('Server is running.') });
+app.listen(port, () => { console.log('Running on Port : ', port) });
