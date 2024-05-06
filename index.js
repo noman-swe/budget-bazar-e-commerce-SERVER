ff78d1763d4dc1ba2d202fe8538bda20ebd8f4b0
noman-swe <noman.swe130@gmail.com>
2024-05-06 22:06:07 +0600

    Backend Code for Pagination is done

--- index.js
+++ index.js
@@ -30,9 +30,17 @@ async function run() {
         // CRUD 
         // READ :
         app.get('/product', async (req, res) => {
+            // console.log('query', req.query);
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size);
             const query = {};
             const cursor = productsCollection.find(query);
-            const products = await cursor.toArray();
+            let products;
+            if (page || size) {
+                products = await cursor.skip(page * size).limit(size).toArray();
+            } else {
+                products = await cursor.toArray();
+            }
             res.send(products);
         });
         // READ EACH :
@@ -81,10 +89,10 @@ async function run() {
         });
 
         // DB collection Data Count
-        app.get('/pagination', async (req, res) => {
+        app.get('/productCount', async (req, res) => {
             const query = {};
-            const cursor = productsCollection.find(query);
-            const count = await cursor.count();
+            // const cursor = productsCollection.find(query);
+            const count = await productsCollection.countDocuments(query);
             res.send({ count });
         })
 
